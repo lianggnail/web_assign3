9d45dfe08831d83398ecb814a43a4b9770848908
Andy <32403425+Sylvartore@users.noreply.github.com>
2017-11-05 13:13:13 -0800

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,12 +1,22 @@
 <?php
 
-  /*if (isset($_POST['login']) && !empty($_POST['username']) 
+  /*
+   $str = file_get_contents("credentials.config");
+  $passwords = explode(' ',$str);
+  for ($i=0; $i<count($passwords); $i++) {
+      if ($i%2==0) {
+          $username=passwords[$i];
+      } else {
+          $password=passwords[$i];
+      }
+  } 
+  if (isset($_POST['login']) && !empty($_POST['username']) 
      && !empty($_POST['password'])) {
-     if ($_POST['username'] == '123' && 
-        $_POST['password'] == '333') {
+     if ($_POST['username'] == $username && 
+        $_POST['password'] == $password) {
         $_SESSION['valid'] = true;
         $_SESSION['timeout'] = time();
-        $_SESSION['username'] = '123';
+        $_SESSION['username'] = $username;
         
         echo 'You have entered valid use name and password';
      }else {
