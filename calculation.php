41eb661aa6b2a9ad4dadb2b17e7ba85f6019ccce
Liang Wu <94liangwu@gmail.com>
2017-11-05 16:58:11 -0800

    comments

--- calculation.php
+++ calculation.php
@@ -4,28 +4,37 @@
     $score = $_POST['score'];
     $total = $_POST['total'];
 
+    //redirect immediately if the answer is empty or not a number
     if (empty($answer) || !is_numeric($answer)){
             $msg='You must enter a number for your answer.';
-        } else {
-            $total ++;
+    } 
+        
+    else {
+        $total ++;
 
-            if ($sign == '+'){
-                $result = $rand1 + $rand2;
-            } else {
-                $result = $rand1 - $rand2;
-            }
+        //calculation bases on different signs
+        if ($sign == '+'){
+             $result = $rand1 + $rand2;
+        } 
+        else {
+            $result = $rand1 - $rand2;
+        }
 
-            if ($result == $answer){
-                $score++;
-                $msg= 'correct';
-            }else{
-                $msg='incorrect, ' . "$rand1" . ' ' . $sign .' ' . $rand2 . ' is ' . $result;
-            }
+        if ($result == $answer){
+            $score++;
+            $msg= 'correct';
+        }
+        //display the question and correct answer
+        else{
+            $msg='incorrect, ' . "$rand1" . ' ' . $sign .' ' . $rand2 . ' is ' . $result;
+        }
     }
 
+    //update all session varibales
     $_SESSION['msg'] = $msg;
     $_SESSION['score'] = $score;
     $_SESSION['total'] = $total;
+
     header('Location:index.php');
     die();
 ?>
--- index.php
+++ index.php
@@ -7,21 +7,26 @@
 </div>
 
 <?php
+
+    //session variables for updating score and total number of questions 
     $msg = $_SESSION['msg'];
     $score = 0 + $_SESSION['score'];
     $total = 0 + $_SESSION['total'];
 
+    //random generate numbers and operation signs
     $rand1 = rand(0, 50);
     $rand2 = rand(0, 50);
     $signs = array('-', '+');
     $sign = $signs[rand(0, 1)];
 
+    //html displays questions
     echo '<h1>';
         echo "<div class='col-xs-4'>$rand1</div>";
         echo "<div class='col-xs-4'>$sign</div>";
         echo "<div class='col-xs-4'>$rand2</div>";
     echo '</h1>';
 
+    //pass user answer and values needed for calculations to calculation.php
     echo '<form action="calculation.php" method="post">';
         echo "<input type='hidden' name='rand1' value='$rand1' />";
         echo "<input type='hidden' name='rand2' value='$rand2' />";
@@ -34,6 +39,7 @@
 ?>
 
 <?php
+    //display green color if answer is correct(class correct) or red for incorrect
     echo '<h2 ';
     if ($msg == 'correct'){
         echo "class='correct'";
@@ -42,6 +48,7 @@
     }
     echo '>';
     echo "$msg</h2>";
+
     echo "<b>Score: $score / $total</b>"
 ?>
 
